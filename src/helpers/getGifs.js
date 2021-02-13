19b0d2e3f6c9f1c7e87ea8bf155aedfc468b4ea2
santiago luna <kobyj103@gmail.com>
2021-02-13 00:27:05 -0500

    Modified getgifs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,7 +3,7 @@
 export const GetGifs = async( category ) => {
         
     try {
-        const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=1CrnyKB7zhLVnoyiiWu7a6biqxBWHBUe`
+        const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=1CrnyKB7zhLVnoyiiWu7a6biqxBWHBUe`
 const resp = await fetch( url );
 const {data} = await resp.json();
 
